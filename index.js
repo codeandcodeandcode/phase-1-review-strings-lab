142868b77f531f5db95d81232c411b00ebb2c1f4
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-10 09:51:17 +0300

    All Tests Passing

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const currentUser = 'Grabs Hobsters';
 //Defining currentUser
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}`;
+
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
